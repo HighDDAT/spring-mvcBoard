ccbd54d03fc821c6792d0b5766b97255188ab760
HighDDAT <highddatddat@gmail.com>
2023-09-28 22:54:04 +0900

    10th commit spring mvc-board 에러/예외 페이지 연습

---
+++ src/main/java/com/spring/mvcboard/commons/exception/CommonExceptionAdvice.java
@@ -0,0 +1,25 @@
+package com.spring.mvcboard.commons.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class CommonExceptionAdvice {
+
+    private static final Logger log = LoggerFactory.getLogger(CommonExceptionAdvice.class);
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView commonException(Exception e) {
+
+        log.info(e.toString());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("/commons/common_error");
+        modelAndView.addObject("exception", e);
+
+        return modelAndView;
+    }
+
+}

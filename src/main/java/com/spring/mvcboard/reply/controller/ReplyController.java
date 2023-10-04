da288ac874daf5ff41d758df2adb6ee957900af3
HighDDAT <highddatddat@gmail.com>
2023-10-04 19:04:58 +0900

    26th commit spring mvc-board 댓글기능 RestController 구현

--- src/main/java/com/spring/mvcboard/reply/controller/ReplyController.java
+++ src/main/java/com/spring/mvcboard/reply/controller/ReplyController.java
@@ -1,9 +1,12 @@
 package com.spring.mvcboard.reply.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,9 +41,45 @@ public class ReplyController {
     }
     
     // 댓글 목록
+    @RequestMapping(value = "/all/{articleNo}", method = RequestMethod.GET)
+    public ResponseEntity<List<ReplyVO>> list(@PathVariable("articleNo") Integer articleNo) {
+        ResponseEntity<List<ReplyVO>> entity = null;
+        try {
+            entity = new ResponseEntity<>(replyService.getReplies(articleNo), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
     
     // 댓글 수정
+    @RequestMapping(value = "/{replyNo}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<String> update(@PathVariable("replyNo") Integer replyNo, @RequestBody ReplyVO replyVO) {
+        ResponseEntity<String> entity = null;
+        try {
+            replyVO.setReplyNo(replyNo);
+            replyService.modifyReply(replyVO);
+            entity = new ResponseEntity<>("modSuccess", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
     
     // 댓글 삭제
+    @RequestMapping(value = "/{replyNo}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> delete(@PathVariable("replyNo") Integer replyNo) {
+        ResponseEntity<String> entity = null;
+        try {
+            replyService.removeReply(replyNo);
+            entity = new ResponseEntity<>("delSuccess", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
+    }
 
 }

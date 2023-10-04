058ffe4577e085084018b26f3d0087eb1a502030
HighDDAT <highddatddat@gmail.com>
2023-10-04 20:01:17 +0900

    31st commit spring mvc-board AOP 적용

---
+++ src/main/java/com/spring/mvcboard/commons/aop/LogAdvice.java
@@ -0,0 +1,53 @@
+package com.spring.mvcboard.commons.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+public class LogAdvice {
+	private static final Logger log = LoggerFactory.getLogger(LogAdvice.class);
+
+	 @Around("execution(* com.doubles.mvcboard..*Controller.*(..))"
+	            + " or execution(* com.doubles.mvcboard..service..*Impl.*(..))"
+	            + " or execution(* com.doubles.mvcboard..persistence..*Impl.*(..))")
+	    public Object logPrint(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+	        long start = System.currentTimeMillis();
+
+	        Object result = proceedingJoinPoint.proceed();
+
+	        String type = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+	        String name = "";
+
+	        if (type.contains("Controller")) {
+	            name = "Controller : ";
+	        } else if (type.contains("Service")) {
+	            name = "Service : ";
+	        } else if (type.contains("DAO")) {
+	            name = "Persistence : ";
+	        }
+
+	        long end = System.currentTimeMillis();
+
+	        log.info(name + type + "."+proceedingJoinPoint.getSignature().getName() + "()");
+	        log.info("Argument/Parameter : " + Arrays.toString(proceedingJoinPoint.getArgs()));
+	        if (result != null) {
+	            log.info("Return Value : " + result.toString());
+	        } else {
+	            log.info("Return Type : void");
+	        }
+	        log.info("Running Time : " + (end-start));
+	        log.info("----------------------------------------------------------------");
+
+	        return result;
+	    }
+
+	}
